2254a70676656ce27ca59980410326beab40152a
Meghdad Hadidi <meghdad.1987@gmail.com>
2024-05-17 16:43:20 +0200

    first functional solution, update README

--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-countdown',
+  selector: 'event-countdown',
   standalone: true,
   imports: [],
   templateUrl: './countdown.component.html',
   styleUrl: './countdown.component.scss'
 })
-export class CountdownComponent {
+export class CountdownComponent implements OnInit {
+  // hardcoding the current date for now
+  // TODO: create a form in parent component
+  // and pass as input
+  targetDate: Date = new Date('2024-06-21T00:00:00')
 
+  // let's keep the countdown as a formatted string 
+  countdown: string = '' // '00 days, 00 h, 00 m, 00 s'
+
+  ngOnInit(): void {
+    // TODO: cleanup before unmount
+    setInterval(() => this.updateCountdown(), 1000)
+  }
+
+  private updateCountdown(): void {
+    const currentTime = new Date();
+    const diff = this.targetDate.getTime() - currentTime.getTime()
+
+    const dayInMillis = 1000 * 60 * 60 * 24
+    const hourInMillis = 1000 * 60 * 60
+    const minuteInMillis = 1000 * 60
+
+    if(diff > 0) {
+      const days = Math.floor(diff / dayInMillis)
+      const hours = Math.floor((diff % dayInMillis) / hourInMillis)
+      const minutes = Math.floor((diff % hourInMillis) / minuteInMillis)
+      const seconds = Math.floor((diff % minuteInMillis) / 1000)
+      this.countdown = `${days} days, ${hours} h, ${minutes} m, ${seconds} s`
+    } else {
+      this.countdown = "It's time!"
+    }
+  }
 }
